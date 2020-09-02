9b3771645b2419c06140a3d5c28cf4e82aa5cad5
John Ferris <junkyardjojo@hotmamil.com>
2020-09-01 19:55:48 -0500

    Adding util functions for use

---
+++ plexinfo/plexutils.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+import traceback
+
+
+def connectPlexServer(plexAcct, svrName):
+    """Will connect to the server
+    plexAcct : MyPlexAccount object
+    svrName  : Plex server name
+
+    Returns : Plex server connection object"""
+    print(f"Connecting to server: {svrName}")
+    try:
+        plexConnect = plexAcct.resource(svrName).connect()
+    except:
+        print("Connection FAILED")
+        print(traceback.format_exc())
+        sys.exit(1)
+
+    return plexConnect
+
+
+def dump_movieTypeLibAtt(movieLib):
+    """Print attributes of the movie Library Type"""
+    print(f"  Library Title: {movieLib.title}")
+    print(f"  Settings:  {movieLib.settings()}")
+    print(f"  it is a      : {movieLib.type} type")
+    print(f"  items        : {movieLib.totalSize}")
+    print(f"  Agent        : {movieLib.agent}")
+    print(f"  Key          : {movieLib.key}")
+    print(f"  Locations    : {movieLib.locations}")
+    print(f"  Scanner      : {movieLib.scanner}")
+
+
+if __name__ == '__main__':
+    pass
