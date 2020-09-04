ed2425f006dfd5e3c3947f23f0654684c2a0ff19
John Ferris <junkyardjojo@hotmamil.com>
2020-09-03 22:11:00 -0500

    Added export a movie library to csv file.

--- plexinfo/plexutils.py
+++ plexinfo/plexutils.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import traceback
+import csv
 
 logger = logging.getLogger("PlexUtils")
 
@@ -32,6 +33,52 @@ def dump_movieLibAtt(movieLib):
     print(f"  Key          : {movieLib.key}")
     print(f"  Locations    : {movieLib.locations}")
     print(f"  Scanner      : {movieLib.scanner}")
+
+
+def csvExportMovie(movieLib, outFile, maxItems=0):
+    """Export movie library items to csv file
+    PARMS:
+    movieLib   : some type of class
+    outFile    : csv output file
+    maxItems  : (optional) max records to save
+
+    Returns the number of items written
+    """
+
+    # Columns layout.
+    colHdr = ['LibName', 'guid', 'Title', 'TitleSort',
+              'OrigTitle', 'Year', 'Location', 'Genres', 'Media']
+
+    # Get all media from the movie library
+    logger.info(f"Getting all movies from the {movieLib.title}")
+    mList = movieLib.all()
+    itemCount = 1
+    if maxItems > 0:
+        logging.warning(f"Max items that will be returned: {maxItems}")
+    logger.info(f"Writing to {outFile}")
+    outcsv = open(outFile, 'w', newline='')
+    with outcsv:
+        csvWrite = csv.DictWriter(outcsv, fieldnames=colHdr)
+        csvWrite.writeheader()
+        for m in mList:
+            itemCount += 1
+            mRow = {}
+            mRow['LibName'] = movieLib.title
+            mRow['guid'] = m.guid
+            mRow['Title'] = m.title
+            mRow['TitleSort'] = m.titleSort
+            mRow['OrigTitle'] = m.originalTitle
+            mRow['Year'] = m.year
+            mRow['Location'] = m.locations
+            mRow['Genres'] = m.genres
+            mRow['Media'] = m.media
+            csvWrite.writerow(mRow)
+
+            if itemCount == maxItems:
+                break
+
+    logger.info(f"Wrote {itemCount} items from {movieLib.title}")
+    print(f"Wrote {itemCount} items from {movieLib.title}")
 
 
 if __name__ == '__main__':
