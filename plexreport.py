b8c39c25826ae5fc337fd5d62c5be935e50ec258
John Ferris <junkyardjojo@hotmamil.com>
2020-09-13 14:37:13 -0500

    Added basic command line options

--- plexreport.py
+++ plexreport.py
@@ -2,7 +2,9 @@ import sys
 import getpass
 import logging.config
 import yaml
+import argparse
 from pathlib import Path
+from datetime import datetime
 
 # app specific modules
 from plexinfo import sqlitedb as mydb
@@ -15,25 +17,29 @@ with open("log.conf", 'rt') as f:
     config = yaml.safe_load(f.read())
 
 logging.config.dictConfig(config)
+VERSION = "1.BETA"
 
 
-def main():
+def main(args):
     plexRptData = Path.cwd() / 'Data'
     plexScripts = Path.cwd() / 'Scripts'
-    userName = "dbjock"
-    collectionCSVFile = "C:/Users/Pops/Downloads/collection_diff.csv"
-    libCSVFile = "C:/Users/Pops/Downloads/movie_diff.csv"
+    now = datetime.now()
+    logger.debug(f"args is {args}")
 
-    logger.debug(f"getpass from user {userName}")
-    userPass = getpass.getpass(prompt=f"Enter {userName}'s Plex Password: ")
-    print(f"Authenticating {userName}")
+    logger.debug(f"getpass from user {args.userName}")
+    userPass = getpass.getpass(
+        prompt=f"Enter {args.userName}'s Plex Password: ")
+    msg = f"Authenticating {args.userName}"
+    print(msg)
+    logger.info(f"{msg}")
     try:
-        plexAcct = MyPlexAccount(userName, userPass)
+        plexAcct = MyPlexAccount(args.userName, userPass)
     except Exception as err:
         logger.critical(f"Error:  {err}", exc_info=True)
         sys.exit()
 
-    logger.debug(f"username: {userName} authenticated.")
+    logger.info(f"username: {args.userName} authenticated.")
+
     ###################################################
     dbFile = ":memory:"
     db1 = mydb.LocalDB(dbLoc=dbFile)
@@ -41,43 +47,85 @@ def main():
     print("-"*70)
     ############################################################
     # Connecting to Plex Servers
-    server1 = "FerrisFam1"
-    print(f"{server1}: Connecting ")
-    plexServer1 = myutil.connectPlexServer(plexAcct, server1)
+    msg = f"Connecting to servers"
+    print(msg)
+    logger.info(f"{msg}")
+    plexServer1 = myutil.connectPlexServer(plexAcct, args.server1)
 
-    server2 = "FerrisFam2"
-    print(f"{server2}: Connecting ")
-    plexServer2 = myutil.connectPlexServer(plexAcct, server2)
+    msg = f"{args.server1} - Connected"
+    print(msg)
+    logger.info(f"{msg}")
 
-    secName = "Movies"
+    plexServer2 = myutil.connectPlexServer(plexAcct, args.server2)
+    msg = f"{args.server2} - Connected"
+    print(msg)
+    logger.info(f"{msg}")
+
     ############################################################
     # Reporting on Collection differences
+
     print()
-    print(f"{server1}: Exporting collection data for movie library section : {secName}")
-    xmovLib = plexServer1.library.section(secName)
+    print(f"{args.server1}: Exporting collection data from movie library section : {args.secName}")
+    xmovLib = plexServer1.library.section(args.secName)
     myutil.collection2Db(db1, xmovLib, dbSvrNameTag='server1')
 
-    print(f"{server2}: Exporting collection data for movie library section : {secName}")
-    xmovLib = plexServer2.library.section(secName)
+    print(f"{args.server2}: Exporting collection data from movie library section : {args.secName}")
+    xmovLib = plexServer2.library.section(args.secName)
     myutil.collection2Db(db1, xmovLib, dbSvrNameTag='server2')
 
-    print(f"Creating Collection Diff file :{collectionCSVFile}")
-    db1.exportColDiff(collectionCSVFile)
+    csvFilename = f"{args.secName}_collections_{now.strftime('%Y-%m-%d-%H%M')}.csv"
+    if args.dirSave == None:
+        collectionCSVFile = Path.cwd() / csvFilename
+    else:
+        collectionCSVFile = Path(args.dirSave) / csvFilename
 
+    print(f"Creating Collection Diff file {collectionCSVFile}")
+    db1.exportColDiff(collectionCSVFile)
     ############################################################
     # Reporting on movie differences
     print()
-    print(f"{server1}: Exporting movie data for movie library section : {secName}")
-    xmovLib = plexServer1.library.section(secName)
+    print(f"{args.server1}: Exporting movie data from movie library section : {args.secName}")
+    xmovLib = plexServer1.library.section(args.secName)
     myutil.movieLib2Db(db1, xmovLib, 'server1')
 
-    print(f"{server2}: Exporting movie data for movie library section : {secName}")
-    xmovLib = plexServer2.library.section(secName)
+    print(f"{args.server2}: Exporting movie data from movie library section : {args.secName}")
+    xmovLib = plexServer2.library.section(args.secName)
     myutil.movieLib2Db(db1, xmovLib, 'server2')
+
+    csvFilename = f"{args.secName}_library_{now.strftime('%Y-%m-%d-%H%M')}.csv"
+    # Path.cwd
+    if args.dirSave == None:
+        libCSVFile = Path.cwd() / csvFilename
+    else:
+        libCSVFile = Path(args.dirSave) / csvFilename
 
     print(f"Creating Movie Library Diff file :{libCSVFile}")
     db1.exportLibDiff(libCSVFile)
 
 
 if __name__ == '__main__':
-    main()
+    logger.info("========= Starting ===========")
+    msg = (f"Plexreport Version: {VERSION}")
+    logger.info(f"{msg}")
+    parser = argparse.ArgumentParser(description="Plex Diff Report")
+    commandSubparser = parser.add_subparsers(title="Commands", dest='command')
+    parser.add_argument('-u', help='The Plex User ID',
+                        type=str, dest='userName', metavar='plex_userid', required=True)
+
+    # Movie Library command
+    movLib_parser = commandSubparser.add_parser(
+        'movie', help="Get a comparison report for a movie library including it's collections")
+    movLibGroup = movLib_parser.add_argument_group(
+        "Comparison report for a Movie library including Collections")
+    movLibGroup.add_argument('server1', help='First plex server', type=str,
+                             metavar='server_name')
+    movLibGroup.add_argument('server2', help='Second plex server', type=str,
+                             metavar='server_name')
+    movLibGroup.add_argument(
+        'secName', help='Movie library name', metavar='lib_name', type=str)
+
+    movLibGroup.add_argument(
+        '--OutPath', help='Directory path where csv files will be placed. Default is current directory', metavar='DirPath', type=str, dest='dirSave')
+
+    parsedArgs = parser.parse_args()
+    main(parsedArgs)
