3b4e0e9ca8079e930e52c118d50d7c6c8ff0b8cb
John Ferris <junkyardjojo@hotmamil.com>
2020-10-05 18:32:43 -0500

    Added data and script path overrides

--- plexinfo/appconfig.py
+++ plexinfo/appconfig.py
@@ -1,10 +1,16 @@
 # Module to read/set application configuration file
 from configparser import ConfigParser
+from pathlib import Path
 
 # sec_main is the [main] section of the config file.
 sec_compare = dict()
 sec_db = dict()
 sec_server = dict()
+sec_paths = dict()
+
+# Defaults for the values:
+sec_paths['data'] = default = Path.cwd() / 'data'
+sec_paths['scripts'] = Path.cwd() / 'scripts'
 
 
 def loadCfg(cfgFile):
@@ -22,3 +28,7 @@ def loadCfg(cfgFile):
     if cfgParser.has_section('db'):
         for k, v in cfgParser.items('db'):
             sec_db[k] = v
+
+    if cfgParser.has_section('paths'):
+        for k, v in cfgParser.items('paths'):
+            sec_paths[k] = Path(v)
